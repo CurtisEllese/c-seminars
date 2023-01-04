9d734ad93f0bfb54937876a1d48cbc56cc4bc042
CurtisEllese <mama-karina@bk.ru>
2023-01-05 03:38:07 +0600

    Modified Task08

--- task08/Program.cs
+++ task08/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая 
+// 1. на вход принимает число (N), а 
+// 2. на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+
+while (count < num - 1)
+{
+    count += 2;
+    Console.Write($"{count} ");
+};
