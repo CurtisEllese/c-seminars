14c9091d24233c5390889f60a2cd12d040e51407
CurtisEllese <mama-karina@bk.ru>
2023-01-05 01:25:00 +0600

    Modified Task03

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая будет выдавать название дня недели по заданному номеру.
+
+Console.Write("Введите целое число от 1 до 7: ");
+string day = Console.ReadLine();
+
+if (day == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+else if (day == "2")
+{
+    Console.WriteLine("Вторник");
+}
+else if (day == "3")
+{
+    Console.WriteLine("Среда");
+}
+else if (day == "4")
+{
+    Console.WriteLine("Четверг");
+}
+else if (day == "5")
+{
+    Console.WriteLine("Пятница");
+}
+else if (day == "6")
+{
+    Console.WriteLine("Суббота");
+}
+else if (day == "7")
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Неправильно введено число!");
+};
