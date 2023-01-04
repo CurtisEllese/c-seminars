8bb72923db88c3238f527d81095f5fa94ddc445f
CurtisEllese <mama-karina@bk.ru>
2023-01-05 02:27:14 +0600

    Modified Task07

--- task07/Program.cs
+++ task07/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая 
+// 1. принимает на вход трехзначное число и 
+// 2. на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num <= 999 && num >= 100)
+{
+    int lastNum = num % 10;
+    Console.Write($"Последняя цифра числа {num} - {lastNum}");
+}
+else
+{
+    Console.Write("Вы ввели некорректное число!");
+};
