b1bb88ab91242c338c7b086be02fa62c51a6afc5
CurtisEllese <mama-karina@bk.ru>
2023-01-05 02:22:03 +0600

    Changed Task05

--- task05/Program.cs
+++ task05/Program.cs
@@ -2,12 +2,12 @@
 // 1. на вход принимает одно число(N), а 
 // 2. на выходе показывает все целые числа в промежутке от -N до N
 
-Console.Write("Введите любое целое число: ");
+Console.Write("Введите любое натуральное число: ");
 int N = Convert.ToInt32(Console.ReadLine());
 int count = -(N);
 
 while (count <= N)
 {
-    Console.WriteLine(N);
+    Console.Write($"{N} ");
     N -= 1;
 };
