3b5afd909755e54fef511fd2a072ade69511ce5d
CurtisEllese <mama-karina@bk.ru>
2023-01-04 23:53:58 +0600

    Modified Task00

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, 
+// 1. которая на вход принимает число и 
+// 2. выдает  его квадрат.
+
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// int number1 = int.Parse(Console.ReadLine());
+
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
