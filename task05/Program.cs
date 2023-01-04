66027a7a426ac533cd17c312041738d17c12d37f
CurtisEllese <mama-karina@bk.ru>
2023-01-05 01:52:36 +0600

    Modified Task05

--- task05/Program.cs
+++ task05/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая 
+// 1. на вход принимает одно число(N), а 
+// 2. на выходе показывает все целые числа в промежутке от -N до N
+
+Console.Write("Введите любое целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = -(N);
+
+while (count <= N)
+{
+    Console.WriteLine(N);
+    N -= 1;
+};
