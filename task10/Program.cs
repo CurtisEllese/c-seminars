29ff0aec3cdff7c3ac33d31da204408da98c865c
CurtisEllese <mama-karina@bk.ru>
2023-01-08 02:02:28 +0600

    Added Task10

---
+++ task10/Program.cs
@@ -0,0 +1,25 @@
+﻿// Написать программу, которая 
+// 1. принимает на вход трехзначное число и 
+// 2. на выходе показывает вторую цифру этого числа.
+
+// 465 - 6
+// 782 - 8
+// 918 - 1
+
+Console.WriteLine("Введите целое трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int FindSecondNum(int num)
+{
+    int secondDigit = num / 10 % 10;
+    return secondDigit;
+}
+
+if (100 <= number && number <= 999)
+{
+    Console.WriteLine($"{number} -> {FindSecondNum(number)}");
+}
+else
+{
+    Console.WriteLine("Введите корректное трехзначное число!");
+}
