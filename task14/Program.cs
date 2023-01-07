90636038225b1b07aa4c4c6355718ca02923b928
CurtisEllese <mama-karina@bk.ru>
2023-01-08 01:05:17 +0600

    Added Task14

---
+++ task14/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу, которая 
+// 1. принимает на вход число и 
+// 2. проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14 - нет
+// 46 - нет
+// 161 - да
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool IsMultiple(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+if (IsMultiple(number))
+{
+    Console.WriteLine($"{number} -> да");
+}
+else
+{
+    Console.WriteLine($"{number} -> нет");
+}
---
+++ task16/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу, которая
+// 1. принимает на вход два числа и 
+// 2. проверяет, является ли одно число квадратом другого.
+
+// 5, 25 - да
+// -4, 16 - да
+// 25, 5 - да
+// 8, 9 - нет
+
+Console.WriteLine("Введите первое целое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+bool IsSquareOfNumber(int number1, int number2)
+{
+    return number1 * number1 == number2 || number2 * number2 == number1;
+}
+
+if (IsSquareOfNumber(firstNumber, secondNumber))
+{
+    Console.WriteLine($"{firstNumber}, {secondNumber} -> да");
+}
+else
+{
+    Console.WriteLine($"{firstNumber}, {secondNumber} -> нет");
+}
