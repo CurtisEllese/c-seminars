b0e3f53bed733f8598c91aade7d6e89387e8b480
CurtisEllese <mama-karina@bk.ru>
2023-01-18 23:31:31 +0600

    Added Task 18

---
+++ task18/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу,
+// 1. которая по заданному номеру четверти, 
+// 2. показывает диапозон возможных координат точек в этой четверти.
+
+
+Console.WriteLine("Введите номер четверти на плоскости от 1 до 4: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string range = Range(quarter);
+Console.WriteLine(range);
+
+
+string Range(int quarterNum)
+{
+    if (quarterNum == 1) return $"Четверти {quarterNum} соответствуют координаты от x > 0, y > 0";
+    if (quarterNum == 2) return $"Четверти {quarterNum} соответствуют координаты от x < 0, y > 0";
+    if (quarterNum == 3) return $"Четверти {quarterNum} соответствуют координаты от x < 0, y < 0";
+    if (quarterNum == 4) return $"Четверти {quarterNum} соответствуют координаты от x > 0, y < 0";
+    return "Введен некорректный номер четверти";
+}
+
