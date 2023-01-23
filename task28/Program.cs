2e2a154e5b3a7b9ac6440a6ad96441054ea5f9c0
CurtisEllese <mama-karina@bk.ru>
2023-01-24 00:25:17 +0600

    Added task 28

---
+++ task28/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу, которая
+// 1. принимает на вход число N и 
+// 2. выдает произведение чисел от 1 до N.
+
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Factorial(number);
+
+if (number >= 0) Console.WriteLine($"{number}! = {factorial}");
+else Console.WriteLine("Введите корректное число!");
+
+int Factorial(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
