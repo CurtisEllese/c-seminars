ad0f7f3ea6f76ad2801e608dac46a180c778b94a
CurtisEllese <mama-karina@bk.ru>
2023-01-24 02:05:15 +0600

    Added task 25

---
+++ task25/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 25: Напишите цикл, который 
+// 1. принимает на вход два числа (A и B) и 
+// 2. возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int firstNum = EnterDigit("Введите первое число: ");
+int secondNum = EnterDigit("Введите натуральное число: ");
+
+while (secondNum < 1)
+{
+    secondNum = EnterDigit("Введите положительное, целое число верно: ");
+}
+
+int numInPower = GetNumInPower(firstNum, secondNum);
+Console.WriteLine($"{firstNum}, {secondNum} -> {numInPower}");
+
+int GetNumInPower(int number1, int number2)
+{
+    int result = 1;
+    for (int i = 1; i <= number2; i++)
+    {
+        result *= number1;
+    }
+    return result;
+}
+
+int EnterDigit(string text)
+{
+    Console.Write(text);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
