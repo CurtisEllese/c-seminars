f7ddab7af77e1c0f922d12d99a7610cd8115eb51
CurtisEllese <mama-karina@bk.ru>
2023-02-06 00:34:50 +0600

    Modified Task 40

--- task40/Program.cs
+++ task40/Program.cs
@@ -24,6 +24,5 @@ else
 
 bool CheckTriangleSides(int side1, int side2, int side3)
 {
-    if (side1 + side2 > side3 && side1 + side3 > side2 && side3 + side2 > side1) return true;
-    return false;
+    return side1 + side2 > side3 && side1 + side3 > side2 && side3 + side2 > side1;
 }
