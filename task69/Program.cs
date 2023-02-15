4ba809b13d3809854c849295bdf79bbb39365028
CurtisEllese <mama-karina@bk.ru>
2023-02-15 21:54:56 +0600

    Added Task 69

---
+++ task69/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 69: 
+// 1. Напишите программу, которая на вход принимает два числа A и B, и
+// 2. возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int numberA = GetEnteredNumber("Введите первое число: ");
+int numberB = GetEnteredNumber("Введите второе число: ");
+
+int aInPowerB = GetAToPowerOfB(numberA, numberB);
+Console.WriteLine(aInPowerB);
+
+int GetAToPowerOfB(int a, int b)
+{
+    if (b == 0) return 1;
+    else return a * GetAToPowerOfB(a, b - 1);
+}
+
+int GetEnteredNumber(string text)
+{
+    Console.Write(text);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
